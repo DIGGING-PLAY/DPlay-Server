a75d54f82767ecbdc4ff1ef76a6ffa9d5ebc547e
2hyunjinn <huenjin04@naver.com>
2025-11-05 00:40:31 +0900

    [feat] #16 trackId로 Track 조회 기능 구현

--- src/main/java/org/dplay/server/domain/track/repository/TrackRepository.java
+++ src/main/java/org/dplay/server/domain/track/repository/TrackRepository.java
@@ -3,7 +3,10 @@ package org.dplay.server.domain.track.repository;
 import org.dplay.server.domain.track.entity.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TrackRepository extends JpaRepository<Track, Long> {
+    Optional<Track> findByTrackId(String trackId);
 }
 
 
