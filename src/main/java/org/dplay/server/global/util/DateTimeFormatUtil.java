95dc0f4046aa13e1ce3e40452846904e4db143d2
2hyunjinn <huenjin04@naver.com>
2025-11-04 13:13:28 +0900

    [refactor] #12 DateTimeFormatUtil 인스턴스화를 방지하도록 private 생성자 추가

--- src/main/java/org/dplay/server/global/util/DateTimeFormatUtil.java
+++ src/main/java/org/dplay/server/global/util/DateTimeFormatUtil.java
@@ -1,9 +1,13 @@
 package org.dplay.server.global.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class DateTimeFormatUtil {
 
     public static final DateTimeFormatter DATE = DateTimeFormatter.ofPattern("yyyy-MM-dd");
