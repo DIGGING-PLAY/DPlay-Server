985193ec7c66d879dd9f1617cd7536e71c29792a
2hyunjinn <huenjin04@naver.com>
2025-11-07 01:22:02 +0900

    [feat] #21 좋아요 등록 API 기능 구현

--- src/main/java/org/dplay/server/domain/post/repository/PostLikeRepository.java
+++ src/main/java/org/dplay/server/domain/post/repository/PostLikeRepository.java
@@ -1,7 +1,17 @@
 package org.dplay.server.domain.post.repository;
 
+import org.dplay.server.domain.post.entity.Post;
 import org.dplay.server.domain.post.entity.PostLike;
+import org.dplay.server.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+    /**
+     * 특정 Post와 User로 PostLike가 존재하는지 확인합니다.
+     *
+     * @param post Post 엔티티
+     * @param user User 엔티티
+     * @return 존재 여부
+     */
+    boolean existsByPostAndUser(Post post, User user);
 }
