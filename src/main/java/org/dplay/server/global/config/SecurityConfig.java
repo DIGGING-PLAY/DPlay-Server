02eebc7a9dc86eb71c6cbf3f65a968f30c5f584f
jihyunniiii <jita1125@naver.com>
2025-11-09 11:48:35 +0900

    [chore] #20 세션 관리 정책 설정

--- src/main/java/org/dplay/server/global/config/SecurityConfig.java
+++ src/main/java/org/dplay/server/global/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -30,6 +31,9 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .requestCache(RequestCacheConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .exceptionHandling(exception ->
                 {
                     exception.authenticationEntryPoint(dPlayJwtAuthenticationEntryPoint);
