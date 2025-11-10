7bf9e050031d1526dd02a7ae7216a6f75023db84
jihyunniiii <jita1125@naver.com>
2025-11-10 23:26:34 +0900

    [feat] #17 Fegin Logging 설정

--- src/main/java/org/dplay/server/global/config/FeignClientConfig.java
+++ src/main/java/org/dplay/server/global/config/FeignClientConfig.java
@@ -1,10 +1,16 @@
 package org.dplay.server.global.config;
 
+import feign.Logger;
 import org.dplay.server.DPlayServerApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(basePackageClasses = DPlayServerApplication.class)
 public class FeignClientConfig {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
