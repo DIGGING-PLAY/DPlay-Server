18458f5ab53a6293398387f7a244716f546fc35c
jihyunniiii <jita1125@naver.com>
2025-11-11 22:04:00 +0900

    [chore] #17 email 필드 제거

--- src/main/java/org/dplay/server/domain/auth/openfeign/apple/dto/ApplePublicKey.java
+++ src/main/java/org/dplay/server/domain/auth/openfeign/apple/dto/ApplePublicKey.java
@@ -1,7 +1,7 @@
 package org.dplay.server.domain.auth.openfeign.apple.dto;
 
 public record ApplePublicKey(
-        String kty, String kid, String use, String alg, String n, String e, String email
+        String kty, String kid, String use, String alg, String n, String e
 ) {
 
 }
