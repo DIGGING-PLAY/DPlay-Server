fcb9b82b4b0a0a731f4e1f1a077d61b634175332
jihyunniiii <jita1125@naver.com>
2025-11-14 23:24:03 +0900

    [feat] #37 내 프로필 수정 API 구현

---
+++ src/main/java/org/dplay/server/controller/user/UserController.java
@@ -0,0 +1,42 @@
+package org.dplay.server.controller.user;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.dplay.server.controller.user.dto.ChangeProfileRequest;
+import org.dplay.server.domain.auth.service.AuthService;
+import org.dplay.server.domain.s3.S3Service;
+import org.dplay.server.domain.user.service.UserService;
+import org.dplay.server.global.auth.constant.Constant;
+import org.dplay.server.global.response.ApiResponse;
+import org.dplay.server.global.response.ResponseBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.annotations.NotNull;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final AuthService authService;
+    private final UserService userService;
+    private final S3Service s3Service;
+
+    @PatchMapping("/me")
+    public ResponseEntity<ApiResponse<Void>> changeProfile(
+            @NotNull @RequestHeader(Constant.AUTHORIZATION_HEADER) final String accessToken,
+            @Valid @RequestPart ChangeProfileRequest changeProfileRequest,
+            @RequestPart(value = "profileImg", required = false) MultipartFile profileImg
+    ) throws IOException {
+        Long userId = authService.getUserIdFromToken(accessToken);
+        String profileImgUrl = (profileImg == null) ? null : s3Service.uploadImage(profileImg);
+
+        userService.updateProfileImage(userId, profileImgUrl);
+        userService.updateNickname(userId, changeProfileRequest.nickname());
+
+        return ResponseBuilder.ok(null);
+    }
+}
---
+++ src/main/java/org/dplay/server/domain/user/service/UserService.java
@@ -0,0 +1,40 @@
+package org.dplay.server.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.dplay.server.domain.auth.service.AuthService;
+import org.dplay.server.domain.user.entity.User;
+import org.dplay.server.domain.user.repository.UserRepository;
+import org.dplay.server.global.exception.DPlayException;
+import org.dplay.server.global.response.ResponseError;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final AuthService authService;
+    private final UserRepository userRepository;
+
+    public void updateProfileImage(final Long userId, final String profileImg) {
+        User user = getUserById(userId);
+
+        if (profileImg == null) {
+            return;
+        } else if (profileImg.isEmpty()) {
+            user.updateProfileImg(null);
+        } else {
+            user.updateProfileImg(profileImg);
+        }
+    }
+
+    public void updateNickname(final Long userId, final String nickname) {
+        authService.validateNickname(nickname);
+        User user = getUserById(userId);
+        user.updateNickname(nickname);
+    }
+
+    private User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new DPlayException(ResponseError.USER_NOT_FOUND));
+    }
+}
