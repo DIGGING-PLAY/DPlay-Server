dd12b5da3525021a9d2a00fc246c67dc75e17f03
jihyunniiii <jita1125@naver.com>
2025-11-17 22:05:45 +0900

    [chore] #37 service 추상화 진행

--- src/main/java/org/dplay/server/domain/auth/openfeign/apple/service/AppleService.java
+++ src/main/java/org/dplay/server/domain/auth/openfeign/apple/service/AppleService.java
@@ -1,32 +1,8 @@
 package org.dplay.server.domain.auth.openfeign.apple.service;
 
-import io.jsonwebtoken.Claims;
-import lombok.RequiredArgsConstructor;
 import org.dplay.server.domain.auth.dto.SocialUserDto;
-import org.dplay.server.domain.auth.openfeign.apple.AppleFeignClient;
-import org.dplay.server.domain.auth.openfeign.apple.dto.ApplePublicKeys;
-import org.dplay.server.domain.auth.openfeign.apple.verify.AppleJwtParser;
-import org.dplay.server.domain.auth.openfeign.apple.verify.PublicKeyGenerator;
-import org.springframework.stereotype.Service;
 
-import java.security.PublicKey;
-import java.util.Map;
+public interface AppleService {
 
-@Service
-@RequiredArgsConstructor
-public class AppleService {
-
-    private final AppleFeignClient appleFeignClient;
-    private final AppleJwtParser appleJwtParser;
-    private final PublicKeyGenerator publicKeyGenerator;
-
-    public SocialUserDto getSocialUserInfo(String identityToken) {
-
-        Map<String, String> headers = appleJwtParser.parseHeaders(identityToken);
-        ApplePublicKeys applePublicKeys = appleFeignClient.getApplePublicKeys();
-        PublicKey publicKey = publicKeyGenerator.generatePublicKey(headers, applePublicKeys);
-        Claims claims = appleJwtParser.parsePublicKeyAndGetClaims(identityToken, publicKey);
-        return SocialUserDto.of(claims.get("sub", String.class));
-    }
-
+    SocialUserDto getSocialUserInfo(String identityToken);
 }
--- src/main/java/org/dplay/server/domain/auth/service/AuthService.java
+++ src/main/java/org/dplay/server/domain/auth/service/AuthService.java
@@ -1,104 +1,20 @@
 package org.dplay.server.domain.auth.service;
 
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
 import org.dplay.server.controller.auth.dto.JwtTokenResponse;
 import org.dplay.server.controller.auth.dto.LoginRequest;
 import org.dplay.server.controller.auth.dto.SignupRequest;
-import org.dplay.server.domain.auth.TokenSaver;
 import org.dplay.server.domain.auth.dto.SocialUserDto;
-import org.dplay.server.domain.auth.entity.Token;
-import org.dplay.server.domain.auth.openfeign.apple.service.AppleService;
-import org.dplay.server.domain.auth.openfeign.kakao.service.KakaoService;
-import org.dplay.server.domain.s3.S3Service;
 import org.dplay.server.domain.user.Platform;
-import org.dplay.server.domain.user.entity.User;
-import org.dplay.server.domain.user.service.UserService;
-import org.dplay.server.global.auth.constant.Constant;
-import org.dplay.server.global.auth.jwt.JwtTokenProvider;
-import org.dplay.server.global.exception.DPlayException;
-import org.dplay.server.global.response.ResponseError;
-import org.dplay.server.global.util.NicknameValidator;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@Service
-@RequiredArgsConstructor
-public class AuthService {
+public interface AuthService {
+    JwtTokenResponse login(String providerToken, LoginRequest loginRequest);
 
-    private final KakaoService kakaoService;
-    private final AppleService appleService;
-    private final S3Service s3Service;
-    private final UserService userService;
-    private final TokenSaver tokenSaver;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final NicknameValidator nicknameValidator;
+    JwtTokenResponse signup(String providerToken, SignupRequest signupRequest, MultipartFile profileImg) throws IOException;
 
-    @Transactional
-    public JwtTokenResponse login(final String providerToken, final LoginRequest loginRequest) {
-        SocialUserDto socialUserDto = getSocialInfo(providerToken, loginRequest.platform());
-        boolean isRegistered = userService.existsByProviderIdAndProvider(socialUserDto.platformId(), loginRequest.platform());
+    SocialUserDto getSocialInfo(String providerToken, Platform platform);
 
-        if (isRegistered) {
-            User user = userService.findUserByProviderIdAndProvider(socialUserDto.platformId(), loginRequest.platform());
-            JwtTokenResponse tokens = jwtTokenProvider.issueTokens(user.getUserId());
-            tokenSaver.save(
-                    Token.builder()
-                            .id(user.getUserId())
-                            .refreshToken(tokens.refreshToken())
-                            .build()
-            );
-            return tokens;
-        } else {
-            throw new DPlayException(ResponseError.USER_NOT_FOUND);
-        }
-    }
-
-    @Transactional
-    public JwtTokenResponse signup(final String providerToken, final SignupRequest signupRequest, final MultipartFile profileImg) throws IOException {
-        SocialUserDto socialUserDto = getSocialInfo(providerToken, signupRequest.platform());
-
-        if (userService.existsByProviderIdAndProvider(socialUserDto.platformId(), signupRequest.platform())) {
-            throw new DPlayException(ResponseError.USER_ALREADY_EXISTS);
-        }
-
-        if (userService.existsByNickname(signupRequest.nickname())) {
-            throw new DPlayException(ResponseError.RESOURCE_ALREADY_EXISTS);
-        } else {
-            nicknameValidator.validate(signupRequest.nickname());
-        }
-
-        User user = User.builder()
-                .platformId(socialUserDto.platformId())
-                .platform(signupRequest.platform())
-                .nickname(signupRequest.nickname())
-                .profileImg((profileImg == null) ? null : s3Service.uploadImage(profileImg)).build();
-        userService.save(user);
-
-        JwtTokenResponse tokens = jwtTokenProvider.issueTokens(user.getUserId());
-        tokenSaver.save(Token.builder()
-                .id(user.getUserId())
-                .refreshToken(tokens.refreshToken())
-                .build());
-
-        user.updateRefreshToken(tokens.refreshToken());
-
-        return tokens;
-    }
-
-    private SocialUserDto getSocialInfo(final String providerToken, final Platform platform) {
-        if (platform.toString().equals("KAKAO")) {
-            return kakaoService.getSocialUserInfo(providerToken);
-        } else if (platform.toString().equals("APPLE")) {
-            return appleService.getSocialUserInfo(providerToken);
-        } else {
-            throw new DPlayException(ResponseError.INVALID_PLATFORM_TYPE);
-        }
-    }
-
-    public Long getUserIdFromToken(final String accessToken) {
-        return jwtTokenProvider.getUserIdFromJwt(accessToken.substring(Constant.BEARER_TOKEN_PREFIX.length()));
-    }
+    Long getUserIdFromToken(String accessToken);
 }
