8656680fb53774a4430c5aa058f31638837e3fc7
jihyunniiii <jita1125@naver.com>
2025-11-20 20:56:58 +0900

    [chore] #41 pushOn Null 처리

--- src/main/java/org/dplay/server/controller/notification/dto/NotificationResponse.java
+++ src/main/java/org/dplay/server/controller/notification/dto/NotificationResponse.java
@@ -1,6 +1,9 @@
 package org.dplay.server.controller.notification.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record NotificationResponse(
-        Boolean pushOn
+        @NotNull(message = "pushOn은 필수 값입니다.")
+        boolean pushOn
 ) {
 }
