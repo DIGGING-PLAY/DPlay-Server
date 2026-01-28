d7085f906685f2cb37bd5c448337fca65035c94a
jihyunniiii <jita1125@naver.com>
2026-01-28 20:11:20 +0900

    [refactor] #110 회원탈퇴 API 에러 로그 상세화

--- src/main/java/org/dplay/server/domain/auth/openfeign/apple/service/impl/AppleServiceImpl.java
+++ src/main/java/org/dplay/server/domain/auth/openfeign/apple/service/impl/AppleServiceImpl.java
@@ -2,6 +2,7 @@ package org.dplay.server.domain.auth.openfeign.apple.service.impl;
 
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.dplay.server.domain.auth.dto.SocialUserDto;
 import org.dplay.server.domain.auth.openfeign.apple.AppleFeignClient;
 import org.dplay.server.domain.auth.openfeign.apple.dto.ApplePublicKeys;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Service;
 import java.security.PublicKey;
 import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AppleServiceImpl implements AppleService {
@@ -41,21 +43,31 @@ public class AppleServiceImpl implements AppleService {
 
     @Override
     public void revoke(String authCode) {
+        log.info("[APPLE-REVOKE-START] 애플 회원 탈퇴 시작");
         try {
             String clientSecret = appleClientSecretGenerator.createClientSecret();
+            log.info("[APPLE-REVOKE-MIDDLE-1] clientSecret 생성 완료");
+
             String refreshToken = getRefreshToken(authCode, clientSecret);
+            log.info("[APPLE-REVOKE-MIDDLE-2] refreshToken 발급 완료");
+
             appleFeignClient.revoke(
                     clientId,
                     clientSecret,
                     refreshToken,
                     "refresh_token"
             );
+            log.info("[APPLE-REVOKE-END] 애플 회원 탈퇴 성공");
+
         } catch (Exception e) {
+            log.error("[APPLE-REVOKE-FAIL] 애플 회원 탈퇴 실패", e);
             throw new DPlayException(ResponseError.APPLE_REVOKE_FAILED);
         }
     }
 
     private String getRefreshToken(final String authCode, final String clientSecret) {
+        log.info("[APPLE-TOKEN-START] 애플 토큰 요청 시작");
+
         try {
             AppleTokenDto appleTokenDto = appleFeignClient.getAppleToken(
                     clientId,
@@ -63,8 +75,11 @@ public class AppleServiceImpl implements AppleService {
                     "authorization_code",
                     authCode
             );
+
+            log.info("[APPLE-TOKEN-END] 애플 토큰 요청 성공");
             return appleTokenDto.refreshToken();
         } catch (Exception e) {
+            log.error("[APPLE-TOKEN-FAIL] 애플 토큰 요청 실패", e);
             throw new DPlayException(ResponseError.APPLE_TOKEN_REQUEST_FAILED);
         }
     }
